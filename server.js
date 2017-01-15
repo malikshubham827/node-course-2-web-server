4ff6123272cec3319833c362318de2372d9eb288
malikshubham827 <malikshubham827@gmail.com>
2017-01-15 15:53:55 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+})
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+})
+
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let log = `${now}: ${req.method} , ${req.url}.`
+  console.log(log);
+  fs.appendFile('server.log', log, (err) => {
+    if (err) {
+      console.log('Error logging to file');
+    }
+  });
+  next();
+});
+
+//maintenance
+app.use((req, res, next) => {
+  res.render('maintenance.hbs');
+})
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'My website'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to process the request'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('server up and running on port 3000');
+});
